afdc7c2164822e0bba0f485c0312dc0d170cfd7b
tim_murr <timurlan.ulanuulu@mail.ru>
2026-07-27 21:34:15 -0500

    fixed errors 2

--- providers.tf
+++ providers.tf
@@ -26,3 +26,12 @@ provider "aws" {
     tags = local.common_tags
   }
 }
+
+terraform {
+  backend "s3" {
+    # bucket         = "project-x-state-bucket-staging"  
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraformlock"
+  }
+}
