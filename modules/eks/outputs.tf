f32d1dee7f3b382b449564fa30014f5e5e9702d0
tim_murr <timurlan.ulanuulu@mail.ru>
2026-07-27 23:52:55 -0500

    fixed errors 2

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,30 +1,45 @@
-
-output "eks_cluster_name" {
+output "cluster_name" {
   description = "Name of the EKS cluster."
-  value       = module.eks.cluster_name
+  value       = aws_eks_cluster.this.name
 }
 
-output "eks_cluster_arn" {
+output "cluster_arn" {
   description = "ARN of the EKS cluster."
-  value       = module.eks.cluster_arn
+  value       = aws_eks_cluster.this.arn
 }
 
-output "eks_cluster_endpoint" {
-  description = "Kubernetes API endpoint."
-  value       = module.eks.cluster_endpoint
+output "cluster_endpoint" {
+  description = "Endpoint of the EKS Kubernetes API."
+  value       = aws_eks_cluster.this.endpoint
 }
 
-output "eks_cluster_version" {
+output "cluster_version" {
   description = "Kubernetes version of the EKS cluster."
-  value       = module.eks.cluster_version
+  value       = aws_eks_cluster.this.version
 }
 
-output "eks_cluster_security_group_id" {
+output "cluster_security_group_id" {
   description = "Security group associated with the EKS control plane."
-  value       = module.eks.cluster_security_group_id
+  value       = aws_security_group.cluster.id
 }
 
-output "eks_worker_security_group_id" {
-  description = "Security group assigned to self-managed worker nodes."
-  value       = module.eks.worker_security_group_id
+output "worker_security_group_id" {
+  description = "Security group used by self-managed worker nodes."
+  value       = aws_security_group.workers.id
+}
+
+output "cluster_iam_role_arn" {
+  description = "ARN of the EKS cluster IAM role."
+  value       = aws_iam_role.cluster.arn
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "OIDC issuer URL of the EKS cluster."
+  value       = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64-encoded certificate authority data."
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+  sensitive   = true
 }
