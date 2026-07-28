03481d9433c3e975aa44fa9dccb954ec9bf7972c
tim_murr <timurlan.ulanuulu@mail.ru>
2026-07-27 23:50:36 -0500

    fixed errors

--- outputs.tf
+++ outputs.tf
@@ -12,33 +12,3 @@ output "private_subnet_ids" {
   description = "IDs of the private subnets."
   value       = module.vpc.private_subnet_ids
 }
-
-output "eks_cluster_name" {
-  description = "Name of the EKS cluster."
-  value       = module.eks.cluster_name
-}
-
-output "eks_cluster_arn" {
-  description = "ARN of the EKS cluster."
-  value       = module.eks.cluster_arn
-}
-
-output "eks_cluster_endpoint" {
-  description = "Kubernetes API endpoint."
-  value       = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_version" {
-  description = "Kubernetes version of the EKS cluster."
-  value       = module.eks.cluster_version
-}
-
-output "eks_cluster_security_group_id" {
-  description = "Security group associated with the EKS control plane."
-  value       = module.eks.cluster_security_group_id
-}
-
-output "eks_worker_security_group_id" {
-  description = "Security group assigned to self-managed worker nodes."
-  value       = module.eks.worker_security_group_id
-}
